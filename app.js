b391ab4c66c319d6cdc6872e7c52404642a25668
H298399 <Honeywell0>
2018-10-04 10:57:30 +0530

    Mongo Connection

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const db = require('mongoose');
+db.connect('mongodb://localhost/cart',{ useNewUrlParser: true });
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
